5f29d7becb3ea633c62dd52f23b85fb77254a4bb
Park Doil <poker5323@naver.com>
2022-12-26 14:54:44 +0900

    [Refactor] 코드 주석 추가

--- AutoTurnGame/Assets/Scripts/Player/Player1.cs
+++ AutoTurnGame/Assets/Scripts/Player/Player1.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+ * 참고 사항 : Player1, Player2 스크립트는 세부적인 동작은 동일합니다. 그래서 주석은 Player1에만 달아 두겠습니다.
+ * 코드 리팩토링은 본가를 내려가야하는 상황이라 지금 당장 어렵고 추후에 꼭 리팩토링 하겠습니다.
+ */
+
 public class Player1 : PlayerBasic
 {
     [SerializeField] private Player2 _player2;
@@ -33,6 +38,9 @@ public class Player1 : PlayerBasic
         StartCoroutine(BehaviorGague());
     }
 
+    /// <summary>
+    /// 공격 함수
+    /// </summary>
     public override void Attack()
     {
         base.Attack();
@@ -41,6 +49,10 @@ public class Player1 : PlayerBasic
         StartCoroutine(BehaviorGague());
     }
 
+    /// <summary>
+    /// 피격 판정 계산 함수
+    /// </summary>
+    /// <param name="damage">다른 플레이어가 주는 데미지</param>
     public void Hit(float damage)
     {
         _showDamage = (int)(damage * 100);
@@ -55,6 +67,10 @@ public class Player1 : PlayerBasic
             GameManager.Instance.SomeoneDie();
         }
     }
+
+    /// <summary>
+    /// 피격 데미지 텍스느 효과 연출 코루틴
+    /// </summary>
     private IEnumerator TextPosition()
     {
         _elapsedTime_text = 0f;
@@ -69,6 +85,9 @@ public class Player1 : PlayerBasic
         }
     }
 
+    /// <summary>
+    /// 행동 게이지 코루틴
+    /// </summary>
     private IEnumerator BehaviorGague()
     {
         _elapsedTime_attack = 0f;
@@ -82,8 +101,12 @@ public class Player1 : PlayerBasic
         Attack();
     }
 
+    /// <summary>
+    /// 누군가 죽었을 때 코루틴 멈추는 함수
+    /// </summary>
     private void SomebodyDie_1()
     {
+        //TODO: 누군가 죽었을때 그 막타를 떄린 플레이어의 코루틴이 다 멈추지 않는 현상 있음. 추후 수정 예정
         StopAllCoroutines();
     }
 
